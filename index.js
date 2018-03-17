a89d8a14b9c17136d5b37e005f1f560f729c674b
ryo-a <ryoa.dev@gmail.com>
2018-03-18 01:07:20 +0900

    initial commit of index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+exports.convert = function convert(input) {
+    //TODO
+}
+
+/**
+ * バリデーション
+ * @param  {string} arg バリデーション対象の文字列（口座番号など）
+ * @param  {Number} digit 桁数
+ * @return {Boolean}       適合していればtrueを返す
+ */
+function validateNumber(arg, digit) {
+    if (typeof (arg) === 'string') {
+
+        if (arg.match(/\D/)) {
+            throw new Error(`${arg} には半角数字以外の文字が含まれています`);
+        }
+
+        if (digit) {
+            if (arg.length != digit) {
+                throw new Error(`${arg} は指定された桁数(${digit})と一致しません`);
+            }
+        }
+
+        return true;
+
+    } else {
+        throw new TypeError(`${arg}がstringではありません`);
+    }
+}
